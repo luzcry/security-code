3cb77ae5a5c6c63d19c18d63f6244ca1b4f0bdfc
Maria Rodriguez <mariacarolina@moonshot.partners>
2023-02-20 23:56:48 -0500

    usestate basic prop and state

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+//trabajar con props en  componentes creados como clases
+
 class ClassState extends React.Component {
     render() {
+        
         return (
             <div>
-                <h2>Eliminar ClassState</h2>
+                <h2>Eliminar {this.props.name}</h2>
                 <p>Por favor escribe el código de seguridad</p>
                 <input placeholder='Código de seguridad' />
                 <button>Comprobar</button>
