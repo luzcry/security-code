a2ca40bf25dd6806743a5db0c1e28cb68942042a
Maria Rodriguez <mariacarolina@moonshot.partners>
2023-03-12 00:08:30 -0500

    useeffect for loading state

--- src/UseState.js
+++ src/UseState.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 function UseState({ name }) {
     const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
 
+    
+    React.useEffect(() => {
+        console.log('empezando el efecto');
+
+        if(loading) {
+            setTimeout(() => {
+                console.log('haciendo validacion');
+    
+                setLoading(false)
+                
+                console.log('terminando validacion');
+            }, 3000);
+        }
+    }, [loading]);
+      
     return (
         <div>
             <h2>Eliminar {name}</h2>
@@ -11,8 +27,11 @@ function UseState({ name }) {
             {error && (
                  <p>El código de seguridad es incorrecto</p>
             )}
+            {loading && (
+                 <p>Cargando...</p>
+            )}
             <input placeholder='Código de seguridad' />
-            <button onClick={() => setError(!error)}>Comprobar</button>
+            <button onClick={() => setLoading(true)}>Comprobar</button>
         </div>
     )
 }
